43be8e8c4997e35dc6fa39151b81dfe8abb59cc4
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-06-26 15:36:17 +0100

    added error handling

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,6 +1,11 @@
 describe('Thermostat', function(){
 
-  var thermostat = new Thermostat
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat
+  })
+  
 
   it('Has an initial temperature of 20', () => {
     expect(thermostat.temperature).toEqual(20);
@@ -11,4 +16,17 @@ describe('Thermostat', function(){
     expect(thermostat.temperature).toEqual(21);
   });
 
+  it('Temperature can be decreased by 1', () => {
+    thermostat.downTemp()
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('Minimum temperature is 10 degrees', () => {
+    // for ( let i = 1; i <= 10; i++){
+    //   thermostat.downTemp
+    // }
+    thermostat.temperature = 10
+    
+    expect(function(){thermostat.downTemp()}).toThrow(("Error: Minimum temperature reached"))
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,27 @@
 'use strict';
 
+const MINIMUM_TEMP = 10;
+
 function Thermostat(){
   this.temperature = 20;
 }
 
 Thermostat.prototype.upTemp = function(){
-  console.log("I AM IN THE FUNCTION")
-  console.log(this.temperature)
   this.temperature += 1
-  console.log(this.temperature)
+}
+
+Thermostat.prototype.downTemp = function(){
+  console.log("Temperature into the downtemp: " + this.temperature)
+  if (this.temperature === MINIMUM_TEMP){
+    console.log("about to throw an error")
+    this.minimumLimitError();
+  }
+  else {
+    console.log("whoops i made it through")
+    this.temperature -= 1
+  }
+}
+
+Thermostat.prototype.minimumLimitError = function(){
+  throw "Error: Minimum temperature reached";
 }
